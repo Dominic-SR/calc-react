25649251ca379c59b58ef79dc208291738d45d30
dominic <dominicseduraja3@gmail.com>
2022-10-30 23:32:35 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
 import './App.css';
 
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type , payload}){
+  // eslint-disable-next-line default-case
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        // eslint-disable-next-line no-undef
+        currentOperand: `${currentOperand || ""}${payload}`
+      }
+  }
+}
+
 function App() {
+  const [{currentOperand, prevoisOperand, operation}, dispatch] = useReducer(
+    reducer,
+    {}
+    )  
   return (
     <div className="calculator-grid">
       <div className='output'>
-        <div className='previous-operant'></div>
-        <div className='current-operant'></div>
+        <div className='previous-operant'>{prevoisOperand} {operation}</div>
+        <div className='current-operant'>{currentOperand}</div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
-      <button>%</button>
+      <DigitButton digit="%" dispatch={dispatch}/>
       <button>1</button>
       <button>2</button>
       <button>3</button>
